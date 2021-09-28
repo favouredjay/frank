dbefcbac66c1f86918d47b7bdacf482ebf8ce971
favouredjay <judom.2011@gmail.com>
2021-09-28 13:54:45 +0100

    update

--- frank/settings.py
+++ frank/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gy^2^t$!wp=f0-mz8v!+%mlgv0slu$(iufzdd+d_$6oe%0h8uq'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
